efd0a220b40a987ed7e56d8f1fed01ddf49bfc05
sonyhero <27anton06@gmail.com>
2023-06-18 15:20:19 +0300

    add-SCSS-mixins

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -2,7 +2,6 @@ import s from './Contacts.module.scss'
 import sContainer from '../common/styles/Container.module.scss'
 import {Tittle} from '../common/components/title/Title';
 import React from 'react';
-import {svgContacts} from './svgContacts';
 
 export const Contacts: React.FC = () => {
     return (
@@ -10,34 +9,16 @@ export const Contacts: React.FC = () => {
             <div className={`${sContainer.container} ${s.contactsContainer}`}>
                 <Tittle text={'Contacts'}/>
                 <form className={s.formContainer}>
-                    <div className={s.formGroup}>
-                        <input type="text" name="name" placeholder="Your Name"
-                        />
-                    </div>
+                    <input type={'text'} name={'name'} placeholder={'Your Name'}
+                    />
+                    <input type={'email'} name={'email'} placeholder={'Your E-mail'}/>
+                    <textarea name={'comment'} placeholder={'Type your message'}/>
 
-                    <div className={s.formGroup}>
-                        <input type="email" name="email" placeholder="Your E-mail"/>
-                    </div>
-
-                    <div className={s.formGroup}>
-                        <textarea name="comment" placeholder="Type your message"/>
-                    </div>
                 </form>
-                <div>
-                    <input type="submit" value={'Sand Message'} className={s.submitBtn}/>
-                </div>
-                <h3 className={s.title}>YOU CAN FIND ME HERE</h3>
-                <div className={s.socialContainer}>
-                    <div className={s.social}><a href={'https://github.com/sonyhero'}><img src={svgContacts.github}
-                                                                                           alt="github"/></a></div>
-                    <div className={s.social}><a href={'mailto:27anton06@gmail.com'}><img src={svgContacts.email}
-                                                                                          alt="email"/></a></div>
-                    <div className={s.social}><a href={'https://t.me/o_an_ton'}><img src={svgContacts.telegram}
-                                                                                     alt="telegram"/></a></div>
-                    <div className={s.social}><a href={'https://www.linkedin.com/in/anton-acharatniuk'}><img
-                        src={svgContacts.linkedin} alt="linkedin"/></a>
-                    </div>
-                </div>
+
+                <button type={'submit'} className={s.submitBtn}>Sand Message</button>
+
+
             </div>
         </div>
     )
--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,14 +1,29 @@
 import s from './Footer.module.scss'
 import sContainer from '../common/styles/Container.module.scss'
 import React from 'react';
+import {svgContacts} from '../contacts/svgContacts';
 
 export const Footer: React.FC = () => {
     return (
         <div className={s.footerBlock}>
             <div className={`${sContainer.container} ${s.footerContainer}`}>
                 {/*<Tittle text={'Anton Acharatniuk'} style={textH2White}/>*/}
+                <div>
+                    <div className={s.title}><h3 >YOU CAN FIND ME HERE</h3></div>
+                    <div className={s.socialContainer}>
+                        <div className={s.social}><a href={'https://github.com/sonyhero'}><img src={svgContacts.github}
+                                                                                               alt="github"/></a></div>
+                        <div className={s.social}><a href={'mailto:27anton06@gmail.com'}><img src={svgContacts.email}
+                                                                                              alt="email"/></a></div>
+                        <div className={s.social}><a href={'https://t.me/o_an_ton'}><img src={svgContacts.telegram}
+                                                                                         alt="telegram"/></a></div>
+                        <div className={s.social}><a href={'https://www.linkedin.com/in/anton-acharatniuk'}><img
+                            src={svgContacts.linkedin} alt="linkedin"/></a>
+                        </div>
+                    </div>
+                </div>
 
-                <h6 className={s.titleFoot}>All rights reserved</h6>
+                <div className={s.titleFoot}><h6>All rights reserved</h6></div>
             </div>
         </div>
     )
