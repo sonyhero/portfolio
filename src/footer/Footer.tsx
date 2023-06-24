395631622a229819990272750da6482571186708
sonyhero <27anton06@gmail.com>
2023-06-24 21:14:08 +0300

    lesson3-add-Social-component

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,28 +1,54 @@
 import s from './Footer.module.scss'
 import React from 'react';
 import {svgContacts} from '../contacts/svgContacts';
+import {Social} from './Social/Social';
 
 export const Footer: React.FC = () => {
+
+    const socials = [
+        {
+            id: '1',
+            link: 'https://github.com/sonyhero',
+            img: svgContacts.github,
+            alt: 'github'
+        },
+        {
+            id: '2',
+            link: 'mailto:27anton06@gmail.com',
+            img: svgContacts.email,
+            alt: 'email'
+        },
+        {
+            id: '3',
+            link: 'https://t.me/o_an_ton',
+            img: svgContacts.telegram,
+            alt: 'telegram'
+        },
+        {
+            id: '4',
+            link: 'https://www.linkedin.com/in/anton-acharatniuk',
+            img: svgContacts.linkedin,
+            alt: 'linkedin'
+        },
+    ]
+
+    const mappedSocials = socials.map(s => {
+        return (
+            <Social key={s.id} link={s.link} alt={s.alt} img={s.img}/>
+        )
+    })
+
     return (
         <div className={s.footerBlock}>
             <div className={s.footerContainer}>
                 {/*<Tittle text={'Anton Acharatniuk'} style={textH2White}/>*/}
                 <div>
-                    <div className={s.title}><h3 >YOU CAN FIND ME HERE</h3></div>
+                    <div className={s.title}><h3>YOU CAN FIND ME HERE</h3></div>
                     <div className={s.socialContainer}>
-                        <div className={s.social}><a href={'https://github.com/sonyhero'}><img src={svgContacts.github}
-                                                                                               alt="github"/></a></div>
-                        <div className={s.social}><a href={'mailto:27anton06@gmail.com'}><img src={svgContacts.email}
-                                                                                              alt="email"/></a></div>
-                        <div className={s.social}><a href={'https://t.me/o_an_ton'}><img src={svgContacts.telegram}
-                                                                                         alt="telegram"/></a></div>
-                        <div className={s.social}><a href={'https://www.linkedin.com/in/anton-acharatniuk'}><img
-                            src={svgContacts.linkedin} alt="linkedin"/></a>
-                        </div>
+                        {mappedSocials}
                     </div>
                 </div>
-
-                <div className={s.titleFoot}><h6>All rights reserved</h6></div>
+                <div className={s.titleFoot}><h6>2023 All rights reserved</h6></div>
             </div>
         </div>
     )
